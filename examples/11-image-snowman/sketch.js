1684a56ec5d84a624c10e5f50fafc958e36a9941
ArosSoft <arossoft@gmail.com>
2026-03-15 19:49:17 +0000

    Deploying to gh-pages from @ ArosSoft/p5editor@ff3da7b110801c578cc45227df5a9cf7260f5d56 🚀

---
+++ examples/11-image-snowman/sketch.js
@@ -0,0 +1,18 @@
+let img; // Переменная для хранения изображения
+
+function preload() {
+  // Загружаем изображение (замените URL на свой)
+  img = loadImage('/images/snowman.png');
+}
+
+function setup() {
+  createCanvas(400, 400);
+  noLoop(); // Останавливаем draw, чтобы не перерисовывать постоянно
+}
+
+function draw() {
+  background(220);
+
+  // Отображаем изображение в позиции (0, 0)
+  image(img, 0, 0, 400, 400);
+}
